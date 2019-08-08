29f9e8a2c285d509fed251db7eff3470e7d059fb
Eric Warsaba <ewarsaba@gmail.com>
2019-08-07 18:27:59 -0700

    cleaning up a  lot of stuff

--- nhl200.py
+++ nhl200.py
@@ -4,32 +4,45 @@ import time
 from bs4 import BeautifulSoup
 import re, sys, time, os
 
-NAME = 0
-POSITION = 1
-PRICE = 2
-GAMES = 3
-POINTS = 4
-PS = 5
+INDEX_NAME = 0
+INDEX_PLAYER_URL = 1
+INDEX_POSITION = 2
+INDEX_PRICE = 3
+INDEX_GAMES = 4
+INDEX_GOALS = 5
+INDEX_ASSISTS = 6
+INDEX_POINTS = 7
+INDEX_POINTS_SHARE = 8
+INDEX_WINS = 9
 
-DEFENSEMAN = 'D'
-GOALIE = 'G'
+POSITION_FORWARD = 'F'
+POSITIONS_FORWARD = ['C', 'LW', 'RW', 'F']
+POSITION_DEFENSEMAN = 'D'
+POSITION_GOALIE = 'G'
 
-player_stats = {}
+TEAMS_LIST =["veg", "edm"] 
 
-def getScore(p):
-	if p[GAMES] >= 0:
-		return p[POINTS]
-	else:
-		return 0
-
-def getRosterScore(roster):
-	return sum(getScore(player) for player in roster)
+# ["ana", "ari", "bos", "buf", "cgy", "car", "chi", "col", "cbj",
+# "dal", "det", "edm", "fla", "lak", "min", "mtl", "nsh", "njd", "nyi", "nyr",
+# "ott", "phi", "pit", "sjs", "stl", "tbl", "tor", "van", "veg", "wsh", "wpg"]
 
 
-def filterPlayers(players, num):
+player_stats_cache = {}
+
+def get_player_score(p):
+	return p[INDEX_POINTS] + p[INDEX_WINS]
+
+def get_roster_score(roster):
+	return sum(get_player_score(player) for player in roster)
+
+def get_roster_cost(roster):
+	return sum(player[INDEX_PRICE] for player in roster)
+
+def filter_players(players, num):
 	filtered = []
+	
 	for i in range(0, len(players)):
-		better = 0
+		num_better = 0
 		p1 = players[i]
 		for j in range(0, len(players)):
 			if i == j:
@@ -37,23 +50,22 @@ def filterPlayers(players, num):
 
 			p2 = players[j]
 
-			if p1[PRICE] >= p2[PRICE] and getScore(p1) <= getScore(p2):
-				better += 1
-				# print "{}\t{}".format(p1[NAME], p2[NAME])
+			if p1[INDEX_PRICE] >= p2[INDEX_PRICE] and get_player_score(p1) <= get_player_score(p2):
+				num_better += 1
 
-			if better >= num:
+			if num_better >= num:
 				break
 
-		if better < num:
+		if num_better < num:
 			filtered.append(p1)
 
 	return filtered
 
-
-def selectTeam(forwards, defense, goalies):
+def select_team(forwards, defense, goalies):
 	best_score = -1
 	best_team = None
 
+	# this is ugly as fuck
 	for f1 in range(0, len(forwards[:-2])):
 		forward1 = forwards[f1]
 		for f2 in range(f1 + 1, len(forwards[:-1])):
@@ -63,15 +75,13 @@ def selectTeam(forwards, defense, goalies):
 				for d1, defense1 in enumerate(defense[:-1]):
 					for d2, defense2 in enumerate(defense[(d1 + 1):]):
 						for goalie in goalies:
-
 							players = [forward1, forward2, forward3, defense1, defense2, goalie]
-
-							total_cost = sum(p[PRICE] for p in players)
+							total_cost = get_roster_cost(players)
 
 							if total_cost > 200:
 								continue
 
-							score = sum(getScore(p) for p in players)
+							score = sum(get_player_score(player) for player in players)
 
 							if score > best_score:
 								best_score = score
@@ -79,32 +89,29 @@ def selectTeam(forwards, defense, goalies):
 
 	return best_team
 
-def printTeam(team, roster):
+def print_team(team_name, roster):
 	team_score = 0
 	team_price = 0
 	team_points = 0
 	team_wins = 0
 
-	print team.upper()
-	print "{}\t{}\t{}\t{}".format("Position", "Name", "Cost", "Points/Wins")
+	print team_name.upper()
+	print "{}\t{}\t{}\t{}\t{}\t{}".format("Position", "Name", "Cost", "Points", "Wins", "Score")
 
-	for p in roster:
-		s = getScore(p)
-		price = p[PRICE]
+	for player in roster:
+		score = get_player_score(player)
+		price = player[INDEX_PRICE]
 
-		team_score += s
+		team_score += score
 		team_price += price
-
-		if p[POSITION] == GOALIE:
-			team_wins += s
-		else:
-			team_points += s
+		team_wins += player[INDEX_WINS]
+		team_points += player[INDEX_POINTS]
+			
+		position = player[INDEX_POSITION]
+		if position not in [POSITION_GOALIE, POSITION_DEFENSEMAN]:
+			position = POSITION_FORWARD
 		
-		pos = p[POSITION]
-		if pos not in [GOALIE, DEFENSEMAN]:
-			pos = "F"
-		
-		print "{}\t{}\t${}\t{}".format(pos, p[NAME], price, s)
+		print "{}\t{}\t${}\t{}\t{}\t{}".format(position, player[INDEX_NAME], price, player[INDEX_POINTS], player[INDEX_WINS], score)
 
 
 	print "Cost\t${}".format(team_price)
@@ -113,38 +120,44 @@ def printTeam(team, roster):
 	print "Score\t{:,}".format(team_score)
 
 
-def createTeam(team):
+def create_team(team_name):
 
 	forwards = []
 	defense = []
 	goalies = []
+
 	# team already scraped
-	file = "data/{}.csv".format(team.upper())
+	file = "data/{}.csv".format(team_name.lower())
 	if os.path.isfile(file):
 		f = open(file)
+
 		for line in f:
 			data = line.split(',')
 			name = data[0].strip()
+			player_url = data[1].strip()
 			position = data[2].strip()
 			price = int(data[3])
 			games = int(data[4])
+			goals = int(data[5])
+			assists = int(data[6])
 			points = int(data[7])
-			ps = float(data[8])
+			points_share = float(data[8])
+			wins = int(data[9])
 
-			player = [name, position, price, games, points, ps]
+			player = [name, player_url, position, price, games, goals, assists, points, points_share, wins]
 
-			if position == DEFENSEMAN:
+			if position == POSITION_DEFENSEMAN:
 				defense.append(player)
-			elif position == GOALIE:
+			elif position == POSITION_GOALIE:
 				goalies.append(player)
 			else:
 				forwards.append(player)
 
-		forwards = filterPlayers(forwards, 3)
-		defense = filterPlayers(defense, 2)
-		goalies = filterPlayers(goalies, 1)
+		forwards = filter_players(forwards, 3)
+		defense = filter_players(defense, 2)
+		goalies = filter_players(goalies, 1)
 
-		roster = selectTeam(forwards, defense, goalies)
+		roster = select_team(forwards, defense, goalies)
 		return roster
 	else:
 		return None
@@ -161,7 +174,7 @@ def getStat(stat):
 		else:
 			return stat.next_sibling.next_sibling.contents[0]
 	except:
-		print "COULDN'T GET STAT"
+		# hockey reference has an empty cell for this, move along
 		return 0
 
 def get_goalie_offense(url):
@@ -189,13 +202,16 @@ def get_goalie_offense(url):
 				return {"goals": goals, "assists": assists, "points": points}
 
 
-def getCareerStats(player_name, player_id, url):
-	global player_stats
-	if player_id in player_stats:
-		print "cache hit\t{}".format(player_name)
-		return player_stats[player_id]
+def get_career_stats(player_url):
+	global player_stats_cache
+
+	if player_url in player_stats_cache:
+		print "cache hit\t{}".format(player_url)
+		return player_stats_cache[player_url]
 	else:
-		print "cache miss\t{}".format(player_name)
+		print "cache miss\t{}".format(player_url)
+	
+	url = "https://www.hockey-reference.com{}".format(player_url)
 	response = requests.get(url)
 
 	soup = BeautifulSoup(response.text, "html.parser")
@@ -211,28 +227,30 @@ def getCareerStats(player_name, player_id, url):
 	points_share = float(getStat(tips[5]))
 
 	if position == 'G':
-		goals = 0
-		assists = 0
-		points = int(getStat(tips[1]))
+		goalie_offense = get_goalie_offense(url)
+		goals = goalie_offense["goals"]
+		assists = goalie_offense["assists"]
+		points = goalie_offense["points"]
+		wins = int(getStat(tips[1]))
 	else:
 		goals = int(getStat(tips[1]))
 		assists = int(getStat(tips[2]))
 		points = int(getStat(tips[3]))
+		wins = 0
 
-	stats = [position,games,goals,assists,points,points_share]
-	player_stats[player_id] = stats
+	stats = {"position": position, "games": games, "goals": goals, 
+	"assists": assists, "points": points, "points_share": points_share, "wins": wins}
+
+	player_stats_cache[player_url] = stats
+	
+	# don't hit hockey reference too fast
 	time.sleep(0.5)
 	return stats
 
 
-def getRows(url):
-	response = requests.get(url)
-	soup = BeautifulSoup(response.text, "html.parser")
-	table = soup.findAll("tbody")[0]
-	return table.find_all('tr')
-
-def initializeCache(teams):
-	global player_stats
+def initialize_cache(teams):
+	global player_stats_cache
+	
 	for team in teams:
 		file = "data/{}.csv".format(team.lower())
 		if not os.path.isfile(file):
@@ -243,7 +261,7 @@ def initializeCache(teams):
 		for line in f:
 			data = line.split(",")
 			name = data[0].strip()
-			name_id = data[1].strip()
+			player_url = data[1].strip()
 			position = data[2].strip()
 			price = int(data[3])
 			games = int(data[4])
@@ -251,51 +269,61 @@ def initializeCache(teams):
 			assists = int(data[6])
 			points = int(data[7])
 			points_share = float(data[8])
+			wins = int(data[9])
 
-			stats = [position,games,goals,assists,points,points_share]
-			player_stats[name_id] = stats
+			stats = {"position": position, "games": games, "goals": goals, 
+			"assists": assists, "points": points, "points_share": points_share, "wins": wins}
+			player_stats_cache[player_url] = stats
+
 		f.close()
 
-def scrapeTeam(team):
-	goalie_rows = getRows("https://www.hockey-reference.com/teams/{}/goalies.html".format(team.upper()))
-	skater_rows = getRows("https://www.hockey-reference.com/teams/{}/skaters.html".format(team.upper()))
+def get_rows(url):
+	response = requests.get(url)
+	soup = BeautifulSoup(response.text, "html.parser")
+	table = soup.findAll("tbody")[0]
+	return table.find_all('tr')
+
+def scrape_team(team):
+	goalie_rows = get_rows("https://www.hockey-reference.com/teams/{}/goalies.html".format(team.upper()))
+	skater_rows = get_rows("https://www.hockey-reference.com/teams/{}/skaters.html".format(team.upper()))
 
 	with open("data/{}.csv".format(team.lower()), 'w') as f:
+		
 		for row in (goalie_rows + skater_rows):
+			# these are divider rows, we don't care
 			if len(row) > 30:
 				continue
+
 			cells = row.find_all("td")
 			name = cells[0].get_text().encode('utf-8')
-			name_id = cells[0]['data-append-csv']
+			player_url = cells[0].next_element['href']
 			price = int(cells[4].get_text())
 
+			# maximum price for a player is $195
 			if price > 195:
 				continue
 
-			url = "https://www.hockey-reference.com{}".format(cells[0].next_element['href'])
-			career_stats = getCareerStats(name, name_id, url)
+			career_stats = get_career_stats(player_url)
 
 			if career_stats is None:
+				# somethng went wrong
 				continue
 
-			s = "{},{},{},{},{},{},{},{},{}".format(name, name_id, career_stats[0], price, career_stats[1], career_stats[2], career_stats[3], career_stats[4], career_stats[5])
+			s = "{},{},{},{},{},{},{},{},{},{}".format(name, player_url, career_stats["position"], price, career_stats["games"], career_stats["goals"], career_stats["assists"], career_stats["points"], career_stats["points_share"], career_stats["wins"])
 			f.write("{}\n".format(s))
 
-        
-teams = ["ana", "ari", "bos", "buf", "cgy", "car", "chi", "col", "cbj",
-"dal", "det", "edm", "fla", "lak", "min", "mtl", "nsh", "njd", "nyi", "nyr",
-"ott", "phi", "pit", "sjs", "stl", "tbl", "tor", "van", "veg", "wsh", "wpg"]
 
-initializeCache(teams)
+print TEAMS_LIST
+initialize_cache(TEAMS_LIST)
 
-for team in teams:
+for team in TEAMS_LIST:
 
-	file = "data/{}.csv".format(team.upper())
+	file = "data/{}.csv".format(team.lower())
 	if not os.path.isfile(file):
-		scrapeTeam(team)
+		scrape_team(team)
 
-	roster = createTeam(team)
-	printTeam(team, roster)
+	roster = create_team(team)
+	print_team(team, roster)
 	print ""
 
 
